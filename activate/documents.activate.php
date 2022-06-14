86a88c9ff26d4617691d2deef1160d9a4247b518
LennardKu <104419069+LennardKu@users.noreply.github.com>
2022-06-14 14:53:56 +0200

    correct table import

--- activate/documents.activate.php
+++ activate/documents.activate.php
@@ -15,7 +15,6 @@ if(!sx_standard_table_exist($sx_database_global_values)){
         `file_name` varchar(255) NOT NULL,
         `file_location` varchar(255) NOT NULL,
         `description` varchar(255) NOT NULL,
-        `file_location` varchar(255) NOT NULL,
         `created_at` datetime NOT NULL DEFAULT current_timestamp()
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
       ';
