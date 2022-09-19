386c2f1181bd715f5e79a5b36c62778b56e8db4d
LennardKu <104419069+LennardKu@users.noreply.github.com>
2022-09-19 13:18:03 +0200

    Save

--- ajax/security/update-url.php
+++ ajax/security/update-url.php
@@ -6,7 +6,13 @@ include $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
 
 if (!current_user_can( 'manage_options' )) { echo 'error'; exit;} // No access
 
+
+// Check if value exists 
 $ChangeUrl = new sx_options;
+if(!$ChangeUrl->check_item('LoginUrl')){
+    $ChangeUrl->create_item('LoginUrl','beheer');
+}
+
 if($ChangeUrl->update_item('LoginUrl','value',str_replace('','',htmlspecialchars(trim($_POST['url']))))){
     echo 'success';
     exit;
